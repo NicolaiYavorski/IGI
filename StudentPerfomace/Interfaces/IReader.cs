dd874b9502737697bf8bf5d3a95f32eb05beefaf
Nicolai <58460816+NicolaiYavorski@users.noreply.github.com>
2020-04-14 09:43:17 +0300

    Clearing the project of trash

---
+++ StudentPerfomace/Interfaces/IReader.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace StudentPerfomace.Interfaces
+{
+    public interface IReader
+    {
+        IEnumerable<Exam> Read(string path, ICustomLogger logger);
+    }
+}
---
+++ StudentPerfomace/Program.cs
@@ -0,0 +1,83 @@
+﻿using Autofac;
+using McMaster.Extensions.CommandLineUtils;
+using StudentPerfomace.Extensions;
+using StudentPerfomace.Interfaces;
+using StudentPerfomace.Models;
+using StudentPerfomace.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentPerfomace
+{
+    public class Program
+    {
+        private const string ExcelFormatName = "xlsx";
+        private const string JsonFormatName = "json";
+
+        [Option("-i", Description = "The path of the input file")]
+        public string InputFilePath { get; }
+
+        [Option("-o", Description = "The path of the output file")]
+        public string OutputFilePath { get; set; }
+
+        [Option("-f", Description = "The file format")]
+        public string FileFormat { get; set; }
+
+        public static void Main(string[] args) =>
+            CommandLineApplication.Execute<Program>(args);
+
+        public void OnExecute()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new DiModule());
+            var container = builder.Build();
+
+            var csvReader = container.Resolve<IReader>();
+            var excelWriter = container.ResolveKeyed<IWriter>(WriterType.Excel);
+            var jsonWriter = container.ResolveKeyed<IWriter>(WriterType.Json);
+            var logger = container.Resolve<ICustomLogger>();
+
+            try
+            {
+                IEnumerable<Exam> exams = csvReader.Read(InputFilePath, logger);
+                IReadOnlyCollection<StudentPerfomance> studentsPerfomance = exams.GetAverageStudentMark();
+                IReadOnlyCollection<MarkForSubject> averageSubjectMark = exams.GetAverageSubjectMark();
+
+                OutputFilePath = FormatEditing(OutputFilePath, FileFormat);
+                switch (FileFormat.ToLower())
+                {
+                    case ExcelFormatName:
+                        excelWriter.Write(OutputFilePath, studentsPerfomance, averageSubjectMark, logger);
+                        break;
+                    case JsonFormatName:
+                        jsonWriter.Write(OutputFilePath, studentsPerfomance, averageSubjectMark, logger);
+                        break;
+                    default:
+                        logger.Setup().Error($"{FileFormat} is not supported");
+                        break;
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                logger.Setup().Error(e.Message, e.GetType());
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                logger.Setup().Error(e.Message, e.GetType());
+            }
+        }
+
+        private string FormatEditing(string outputPath, string format)
+        {
+            var separator = '.';
+            var extension = outputPath.Split(separator).Last().ToLower();
+            return extension switch
+            {
+                ExcelFormatName => outputPath,
+                JsonFormatName => outputPath,
+                _ => $"{outputPath}.{format.ToLower()}",
+            };
+        }
+    }
+}
