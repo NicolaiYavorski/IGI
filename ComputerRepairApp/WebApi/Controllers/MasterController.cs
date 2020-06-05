aacf696d554a4f2fa6b438266b682f471da3ac7e
Nicolai <58460816+NicolaiYavorski@users.noreply.github.com>
2020-06-05 17:42:36 +0300

    Task5

---
+++ ComputerRepairApp/WebApi/Controllers/MasterController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using BusinessLogic.DTO;
+using BusinessLogic.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MasterController : ControllerBase
+    {
+        private readonly IMasterService _masterService;
+
+        public MasterController(IMasterService masterService)
+        {
+            _masterService = masterService;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var masters = _masterService.GetAll();
+            return Ok(masters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var order = _masterService.GetById(id);
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public ActionResult Create([FromBody] MasterDto master)
+        {
+            try
+            {
+                _masterService.Add(master);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Edit([FromBody] MasterDto master)
+        {
+            try
+            {
+                _masterService.Update(master);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                _masterService.Delete(id);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+    }
+}
